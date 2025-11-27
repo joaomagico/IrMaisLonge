950a996fd2d95cef5701c1063e3951c7c51465e5
joaomagico <joao.magico@gmail.com>
2025-11-27 01:09:41 +0000

    alinhar paginas

--- website/script.js
+++ website/script.js
@@ -1,3 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const TOKEN_VALIDO = "abc123";
+  //const TOKEN_VALIDO = "Q15qxgcvQImhc1uDYwUIycyK5L3D01AW5cihRTzr3ViTxuVVWv8F13soryEYcYHV";
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get("token");
+
+  if (token !== TOKEN_VALIDO) {
+    document.body.innerHTML = `
+      <div style="
+        background: black;
+        color: red;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.8rem;
+        font-family: Arial, sans-serif;
+        text-align: center;
+        padding: 20px;
+      ">
+        ⛔ Acesso não autorizado
+      </div>
+    `;
+    throw new Error("Token inválido");
+  }
+});
 
 
 document.querySelectorAll('.insta-link').forEach(link => {
